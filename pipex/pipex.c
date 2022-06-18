00805ec77a4ee89de5277a9c2cdb781ba4113155
Laila Chokri <lchokri@e1r5p10.1337.ma>
2022-06-18 20:16:07 +0100

    i think it's fine

--- pipex/pipex.c
+++ pipex/pipex.c
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/14 18:30:12 by lchokri           #+#    #+#             */
-/*   Updated: 2022/06/17 18:39:46 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/06/18 20:14:15 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -41,8 +41,8 @@ char	*check_cmd(char *cmd, char **envp)
 
 void	not_accessible(char **av, char **cmd1, char **cmd2, char **envp)
 {
-	char **argv1;
-	char **argv2;
+	char	**argv1;
+	char	**argv2;
 
 	argv1 = ft_split(av[2], ' ' );
 	argv2 = ft_split(av[3], ' ' );
@@ -62,43 +62,31 @@ void	not_accessible(char **av, char **cmd1, char **cmd2, char **envp)
 		*cmd2 = av[3];
 }
 
-void	manage_err(char *ptr, char *ptr2)
+void	manage_err()
 {
-	if (ptr != NULL)
-		free(ptr);
-	if (ptr2 != NULL)
-		free(ptr);
 	perror("Error");
 	exit(EXIT_FAILURE);
 }
 //pipe writes to fd[1]
 int main(int ac, char **av, char **envp)
 {
-	int		*fd;
-	int		*fd2;
-	int		*end;
+	int		fd[2];
+	int		end[2];
 	int		chld_pid;
 	char	*cmd1;
 	char	*cmd2;
 	char	**argv1;
 	char	**argv2;
 
-	end = malloc(2 * sizeof(int));
-	argv1 = ft_split(av[2], ' ' );
-	argv2 = ft_split(av[3], ' ' );
 	if (ac == 5)
 	{
-		fd = malloc(2 * sizeof(int));
+		argv1 = ft_split(av[2], ' ' );
+		argv2 = ft_split(av[3], ' ' );
 		if ((fd[0] = open(av[1], O_RDONLY))== -1)
-			manage_err(NULL, NULL);
+			manage_err();
 		if ((fd[1] = open(av[4], O_RDWR | O_TRUNC | O_CREAT, 0666))== -1)
-			manage_err(NULL, NULL);
+			manage_err();
 		not_accessible(av, &argv1[0], &argv2[0], envp);
-		if (!argv1[0])
-			manage_err(NULL, NULL);
-		if (!argv2[0])
-			manage_err(NULL, NULL);
-		printf("%s\n", argv1[0]);
 		pipe(end);
 		chld_pid = fork();
 		if (chld_pid == 0)
@@ -107,17 +95,18 @@ int main(int ac, char **av, char **envp)
 			dup2(fd[0], 0);
 			dup2(end[1], 1);
 			execve(argv1[0], argv1, envp);
-		
 		}
 		else
-			close(end[1]);
+		{
 			wait(NULL);
+			close(end[1]);
+		}
 		chld_pid = fork();
 		if (chld_pid == 0)
 		{
 			close(end[1]);
 			dup2(end[0], 0);
-			dup2(1, fd[1]);
+			dup2(fd[1], 1);
 			execve(argv2[0], argv2, envp);
 			printf("we're in child 2\n");
 		}
@@ -126,7 +115,6 @@ int main(int ac, char **av, char **envp)
 			wait(NULL);
 			printf("we're back to parent 2\n");
 		}
-			close(fd2[0]);
 	}
 	else
 		write(2, "Error!\nUsage: ./pipex f1 cmd1 cmd2 f2\n", 41);
