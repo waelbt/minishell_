d870325c3592200a9ea2ebf769e54aa2b8043d48
Laila Chokri <lchokri@e1r11p12.1337.ma>
2022-06-22 13:42:17 +0100

    builtin start

---
+++ src/built_ins/built_ins.c
@@ -0,0 +1,57 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   built_ins.c                                        :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2022/06/21 19:48:57 by lchokri           #+#    #+#             */
+/*   Updated: 2022/06/22 13:42:01 by lchokri          ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "../include/minishell.h"
+
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+void		echo(char **after_expand)
+{
+	int		i;
+	int		check;
+	char	*str;
+
+	i = 1;
+	check = 0;
+	str = NULL;
+	if (!ft_strncmp(after_expand[1], "-n", 3))
+	{
+		check = 1;
+		i = 2;
+	}
+	str = ft_strjoin(after_expand[i], " ");
+	while(after_expand[++i])
+	{
+		printf("%s\n", str);
+		str = ft_strjoin(str, after_expand[i]);
+		str = ft_strjoin(str, " ");
+	if (!ft_strncmp(after_expand[1], "-n", 3))
+//		i++;
+	}
+	printf("%s", str);
+	free (str);
+	if (check == 0)
+		printf("\n");
+}
+int main(int ac, char **av)
+{
+	(void)ac;
+	echo(av);
+}
