b19710d2971d37b35f9211649017dc668d68bc19
laila-chk <lyly.2356@gmail.com>
2022-06-22 20:00:06 +0100

    pwd

--- src/built_ins/pwd.c
+++ src/built_ins/pwd.c
@@ -1,15 +1,5 @@
-
-
-
 void	pwd(char **envp)
 {
-		//still need to do smth about s size
-	char s[111];
-
-	printf("%s\n", getcwd(s, 111));
+	printf("%s\n", getcwd(NULL, 0));
 }
 
-int main(int ac, char **av, char **envp)
-{
-		pwd(envp);
-}
