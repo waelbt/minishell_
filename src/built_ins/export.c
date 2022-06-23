ceb1a300e7f4c2c24b81b6c8bb191ebfd3218d42
laila-chk <lyly.2356@gmail.com>
2022-06-23 21:11:20 +0100

    export

--- src/built_ins/export.c
+++ src/built_ins/export.c
@@ -1,19 +1,69 @@
-
+#include "../include/minishell.h"
+#include <string.h>
 #include <stdio.h>
 #include <unistd.h>
 #include <stdlib.h>
 
-void	export(char **envp, char value)
+void	free_double_char(char **tmp)
+{
+	int	i;
+
+	i = 0;
+	if(!tmp)
+		return ;
+	while (tmp[i])
+	{
+		free(tmp[i]);
+		i++;
+	}
+	free(tmp);
+}
+/* strduppp*/
+void	print_env(char **envp)
+{
+	int	i;
+	
+	i = 0;
+	while(envp[i])
+	{
+		printf("**%s\n", envp[i]);
+		i++;
+	}
+}
+
+void	export(char ***envp, char *value)
+{
+	int		i;
+	char	**new_envp;
+
+	i = 0;
+	if (value == NULL)
+		print_env(*envp);
+	else
+	{
+		while ((*envp)[i])
+			i++;
+		new_envp = (char **)malloc((i + 1) * sizeof(char *));
+		i = 0;
+		while ((*envp)[i])
+		{
+			new_envp[i] = strdup((*envp)[i]);
+			i++;
+		}
+		new_envp[i++] = strdup(value);
+		new_envp[i] = NULL;
+//		free_double_char(*envp);
+		*envp = my_envp(new_envp);
+		free_double_char(new_envp);
+	}
+}
+/*zeeed wahd njma a bnti*/
+int main(int ac, char **av)
 {
 	int		i;
 
 	i = 0;
-	if (value = NULL)
-		print_env(envp);
-	else
-	{
-		while (envp[i])
-			i++;
-
-	}
+	export(&av, "some var");
+	while(av[i])
+		printf("```%s```\n", av[i++]);
 }
