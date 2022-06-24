9ca0c02523d4cc04d3be63d349f9fa7c512caf69
laila-chk <lyly.2356@gmail.com>
2022-06-24 16:48:39 +0100

    welllll..

--- src/built_ins/export.c
+++ src/built_ins/export.c
@@ -16,6 +16,43 @@
 #include <unistd.h>
 #include <stdlib.h>
 
+int	ft_strlen(char *str)
+{
+	int	i;
+
+	i = 0;
+	while (str[i])
+		i++;
+	return (i);
+}
+
+char	*ft_strjoin(char *s1, char *s2)
+{
+	char	*ptr;
+	size_t	len;
+	size_t	i;
+
+	i = 0;
+	if (!s1 || !s2)
+		return (0);
+	len = ft_strlen(s1) + ft_strlen(s2);
+	ptr = (char *) malloc((len + 1) * sizeof(char));
+	if (!ptr)
+	{
+		return (0);
+	}
+	while (i < len)
+	{
+		if (i < ft_strlen(s1))
+			*(ptr + i) = *(s1 + i);
+		else if (i >= ft_strlen(s1))
+			*(ptr + i) = *(s2++);
+		i++;
+	}
+	*(ptr + i) = '\0';
+	return (ptr);
+}
+
 void	free_double_char(char **tmp)
 {
 	int	i;
@@ -43,7 +80,42 @@ void	print_export(char **envp)
 	}
 }
 
-void	export(char ***envp, char *value)
+
+char	*ft_strchr(char *s, int c)
+{
+	size_t		i;
+	char		*p;
+
+	i = 0;
+	p = (char *) s;
+	while (i <= ft_strlen(s))
+	{
+		if (*(p + i) == (char )c)
+			return (p + i);
+		i++;
+	}
+	return (0);
+}
+
+		//value should be splited only from the first '=', join 
+		//double quotes in the beg and ending pejjj so annoying
+
+void	right_value(char **str)
+{
+	char	*s;
+	char	*tmp;
+
+	if ((s = ft_strchr(*str, '=')))
+		{
+			tmp = ft_strjoin("\"", ++s);
+			tmp = ft_strjoin(tmp, "\"");
+		}
+	*str = 
+	*str = strdup(tmp);
+	printf("str === %s\n", *str);
+}
+	
+void	my_export(char ***envp, char *value)
 {
 	int		i;
 	char	**new_envp;
@@ -53,6 +125,10 @@ void	export(char ***envp, char *value)
 		print_export(*envp);
 	else
 	{
+		printf("val before: %s\n", value);
+		right_value(&value);
+		printf("-------------------------------------------------------\n");
+		printf("val after: %s\n", value);
 		while ((*envp)[i])
 			i++;
 		new_envp = (char **)malloc((i + 1) * sizeof(char *));
@@ -62,8 +138,9 @@ void	export(char ***envp, char *value)
 			new_envp[i] = strdup((*envp)[i]);
 			i++;
 		}
-		new_envp[i++] = strdup(value);
-		new_envp[i] = NULL;
+		new_envp[i] = strdup(value);
+		new_envp[++i] = NULL;
+		/*old envp need to be freed*/
 		*envp = my_envp(new_envp);
 	}
 }
@@ -72,8 +149,9 @@ int main(int ac, char **av)
 {
 	int		i;
 
+	(void)ac;
 	i = 0;
-	export(&av, "some var");
+	my_export(&av, "some=var");
 	while(av[i])
 	{
 		printf("```%s```\n", av[i]);
