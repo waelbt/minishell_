adcbde8fede00cfb2b4296e376da447e71e71f6f
Laila Chokri <lchokri@e1r13p6.1337.ma>
2022-06-25 16:35:45 +0100

    my export

--- src/built_ins/export.c
+++ src/built_ins/export.c
@@ -6,7 +6,7 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/23 22:13:02 by lchokri           #+#    #+#             */
-/*   Updated: 2022/06/24 20:30:43 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/06/25 15:33:22 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,119 +26,12 @@ int		get_j(char *str)
 	return (i);
 }
 
-int	ft_strlen(char *str)
-{
-	int	i;
-
-	i = 0;
-	while (str[i])
-		i++;
-	return (i);
-}
-
-char	*ft_strjoin(char *s1, char *s2)
-{
-	char	*ptr;
-	size_t	len;
-	size_t	i;
-
-	i = 0;
-	if (!s1 || !s2)
-		return (0);
-	len = ft_strlen(s1) + ft_strlen(s2);
-	ptr = (char *) malloc((len + 1) * sizeof(char));
-	if (!ptr)
-	{
-		return (0);
-	}
-	while (i < len)
-	{
-		if (i < ft_strlen(s1))
-			*(ptr + i) = *(s1 + i);
-		else if (i >= ft_strlen(s1))
-			*(ptr + i) = *(s2++);
-		i++;
-	}
-	*(ptr + i) = '\0';
-	return (ptr);
-}
-
-void	free_double_char(char **tmp)
-{
-	int	i;
-
-	i = 0;
-	if(!tmp)
-		return ;
-	while (tmp[i])
-	{
-		free(tmp[i]);
-		i++;
-	}
-	free(tmp);
-}
-/* strduppp*/
-void	print_export(char **envp)
-{
-	int	i;
-	
-	i = 0;
-	while(envp[i])
-	{
-		printf("declare -x %s\n", envp[i]);
-		i++;
-	}
-}
-
-char	*ft_substr(char *s, unsigned int start, size_t len)
-{
-	char	*p;
-	size_t	i;
-	size_t	slen;
-
-	if (!s)
-		return (NULL);
-	slen = ft_strlen(s);
-	if (start >= slen)
-		len = 0;
-	i = 0;
-	if (len < slen)
-		p = (char *)malloc((len + 1) * sizeof(char));
-	else
-		p = (char *)malloc((slen + 1) * sizeof(char));
-	if (!p)
-		return (NULL);
-	while (s[start] && i < len)
-	{
-		p[i] = s[start];
-		i++;
-		start++;
-	}
-	p[i] = '\0';
-	return (p);
-}
-
-char	*ft_strchr(char *s, int c)
-{
-	size_t		i;
-	char		*p;
-
-	i = 0;
-	p = (char *) s;
-	while (i <= ft_strlen(s))
-	{
-		if (*(p + i) == (char )c)
-			return (p + i);
-		i++;
-	}
-	return (0);
-}
 char **sorted_env(char **env)
 {
 	int		i;
 	int		j;
 	int		size;
-	char	temp[5000];	
+	char	*temp;	
 	
 	i = 0;
 	j = 0;
@@ -150,11 +43,11 @@ char **sorted_env(char **env)
 		j = 0;
 		while (j < size -1 - i)
 		{
-			if(strcmp(env[j], env[j+1]) > 0)
+			if(ft_strcmp(env[j], env[j+1]) > 0)
 			{
-				strcpy(temp, env[j]);
-				strcpy(env[j], env[j+1]);
-				strcpy(env[j+1], temp);
+				temp = ft_strdup(env[j]);
+				env[j] = ft_strdup(env[j + 1]);
+				env[j + 1] = ft_strdup(temp);
 			}
 			j++;
 		}
@@ -162,6 +55,20 @@ char **sorted_env(char **env)
 	}
 	return (env);
 }
+
+void	print_export(char **envp)
+{
+	int	i;
+	
+	i = 0;
+	envp = sorted_env(envp);
+	while(envp[i])
+	{
+		printf("declare -x %s\n", envp[i]);
+		i++;
+	}
+}
+
 void	right_value(char **str)
 {
 	char	*s;
@@ -174,7 +81,7 @@ void	right_value(char **str)
 	if ((s = ft_strchr(*str, '=')))
 		{
 			tmp = ft_strjoin("\"", ++s);
-			tmp2 = *str;
+	//		tmp2 = *str;
 			tmp1 = tmp;
 			tmp = ft_strjoin(tmp, "\"");
 			j = get_j(*str);
@@ -186,7 +93,7 @@ void	right_value(char **str)
 		}
 	else
 	{
-		tmp = strdup("");
+		tmp = ft_strdup("");
 		*str = ft_strjoin(*str, tmp);
 		free(tmp);
 	}
@@ -205,35 +112,34 @@ void	my_export(char ***envp, char *value)
 		right_value(&value);
 		while ((*envp)[i])
 			i++;
-		new_envp = (char **)calloc((i + 2), sizeof(char *));
+		new_envp = (char **)malloc((i + 2) * sizeof(char *));
 		i = 0;
 		while ((*envp)[i])
 		{
-			new_envp[i] = strdup((*envp)[i]);
+			new_envp[i] = ft_strdup((*envp)[i]);
 			i++;
 		}
-		new_envp[i] = strdup(value);
+		new_envp[i] = ft_strdup(value);
 		i++;
 		new_envp[i] = NULL;
 		*envp = new_envp;
+		*envp = sorted_env(*envp);
 		free(value);
 	}
 }
-
-/* sort table in another table
- * if var hav no val don't add it to env*/
+/*ba9in leaks mcha3ch3in o function d c99 as strdup*/
 int main(int ac, char **av)
 {
 	int		i;
 	char	**str;
 
 	i = 0;
-	sorted_env(av);
+	my_export(&av, "some=");
 	while(av[i])
 	{
 		printf("```%s```\n", av[i]);
 		i++;
 	}
-//	system("leaks a.out");
+	//system("leaks a.out");
 }
 
