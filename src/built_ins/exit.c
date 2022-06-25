5432e70479a9c420dff3b4ed9bd65e51c35ec8af
Laila Chokri <lchokri@e1r5p3.1337.ma>
2022-06-25 19:35:05 +0100

    exit done

--- src/built_ins/exit.c
+++ src/built_ins/exit.c
@@ -6,11 +6,23 @@
 /*   By: lchokri <marvin@42.fr>                     +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/06/25 14:47:52 by lchokri           #+#    #+#             */
-/*   Updated: 2022/06/25 14:52:43 by lchokri          ###   ########.fr       */
+/*   Updated: 2022/06/25 19:29:04 by lchokri          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-void	my_exit(void)
+#include "../include/minishell.h"
+
+void	my_exit(char *str)
 {
-	exit(0);
+	int		status;
+
+	if (str == NULL)
+		exit(0); //0 should be replaced by the global var because : WHEN EXIT HAVE NO PARAMETER, IT TAKES THE EXIT STATUS
+					//OF THE LAST CMD EXECUTED
+	else
+	{
+		status = str_to_num(str);
+		status = (unsigned char)status; //// <-- hada howa lglobal var to be saved
+		exit(status);
+	}
 }
